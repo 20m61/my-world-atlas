a35968a6879eca6ad4e8113a242adb29b48a969b
20m61 <changhwi.chang@gmail.com>
2025-03-30 22:27:44 +0900

    vite.config.jsにビルド設定を追加

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/my-world-atlas/'
+  base: '/my-world-atlas/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    sourcemap: true
+  }
 })
